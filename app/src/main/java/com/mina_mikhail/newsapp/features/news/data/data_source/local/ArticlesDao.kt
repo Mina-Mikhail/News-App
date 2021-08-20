b0b876974fa3fe2c4713bd3ea6aec7cb8106e939
Mina Mikhail <menasamirgerges@gmail.com>
2021-08-20 23:49:30 +0200

    + Code Refactoring

--- app/src/main/java/com/mina_mikhail/newsapp/features/news/data/data_source/local/ArticlesDao.kt
+++ app/src/main/java/com/mina_mikhail/newsapp/features/news/data/data_source/local/ArticlesDao.kt
@@ -10,27 +10,13 @@ import com.mina_mikhail.newsapp.features.news.domain.entity.model.Article
 
 @Dao
 interface ArticlesDao {
-  /**
-   * Select all articles from the articles table.
-   *
-   * @return all articles.
-   */
+
   @Query("SELECT * FROM articles")
   fun getAll(): LiveData<List<Article>>
 
-  /**
-   * Insert articles in the database. If the article already exists, replace it.
-   *
-   * @param article the city to be inserted.
-   */
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend fun insert(article: Article)
 
-  /**
-   * Delete article from the articles table.
-   *
-   * @param article the city to be inserted.
-   */
   @Delete
   suspend fun delete(article: Article)
 }
